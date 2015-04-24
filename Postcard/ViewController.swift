d92d40517649829110c3a256ebad0ca44a96bae9
James Davey <jeimuzu@hotmail.com>
2015-04-24 13:44:55 +0900

    displayInfo functionality added

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -6,20 +6,57 @@
 //  Copyright (c) 2014 James Davey. All rights reserved.
 //
 
+
 import UIKit
+
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var displayInfo: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBOutlet weak var enterNameTextField: UITextField!
+    
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    
+    @IBOutlet weak var mailButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
+    
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        // Code will evaluate when we press the button
+        
+        // Adding a Comment here to tests commits
+        messageLabel.hidden = false
+        messageLabel.text = enterMessageTextField.text
+        messageLabel.textColor = UIColor.redColor()
+        
+        displayInfo.hidden = false
+        displayInfo.text = enterNameTextField.text
+        displayInfo.textColor = UIColor.blueColor()
+        
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
+        
+        enterMessageTextField.text = ""
+        enterMessageTextField.resignFirstResponder()
+        
+        mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
 
+    
+
+        
+    }
+    
 
 }
-
